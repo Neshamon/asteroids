c5ee23556ca94b468401ff9bc72e1749841acb7e
nephesh <jmmatthews@proton.me>
2025-01-03 16:39:07 -0600

    Set dt

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock
+    clock = pygame.time.Clock()
     dt = 0
     while True:
         screen.fill((0, 0, 0))
@@ -14,6 +14,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         clock.tick(60)
+        dt = int(clock.tick(60)) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
