e270641ed9f1cc3903efa2b6046403d1c0daedf4
nephesh <jmmatthews@proton.me>
2025-01-03 16:27:27 -0600

    Track events

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
         pygame.display.flip()
 
